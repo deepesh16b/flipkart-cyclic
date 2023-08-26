2d27810d3cd7c2220a4334f6dd4bd298b40ec2bb
Deepesh Bhardwaj <deepeshbhardwaj58@gmail.com>
2023-08-27 03:22:47 +0530

    initial 5

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.use(
   })
 );
 app.use(express.static(path.join(__dirname, './client/build')))
+
 app.use("*",function(req, res){
   res.sendFile(path.join(__dirname, './client/build/index.html'));
 })
