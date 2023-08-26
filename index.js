eaa815ae568015ef894d7e03bddfcd91042335bf
Deepesh Bhardwaj <deepeshbhardwaj58@gmail.com>
2023-08-27 03:03:57 +0530

    initial 4

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(
   })
 );
 app.use(express.static(path.join(__dirname, './client/build')))
-app.use("*",function(res, req){
+app.use("*",function(req, res){
   res.sendFile(path.join(__dirname, './client/build/index.html'));
 })
 app.use(express.urlencoded({ extended: true }));
