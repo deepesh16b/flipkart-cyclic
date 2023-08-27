85a17ef2185f4711a9f903305e1430cfff6ea70b
Deepesh Bhardwaj <deepeshbhardwaj58@gmail.com>
2023-08-28 00:29:27 +0530

    try 5

--- controller/payment-controller.js
+++ controller/payment-controller.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import Razorpay from 'razorpay';
+import Razorpay from "razorpay";
 import crypto from "crypto";
 
 const hmac_sha256 = (data, secret) => {
@@ -10,15 +10,18 @@ const hmac_sha256 = (data, secret) => {
   return hmac.digest("hex");
 };
 
-
 // import { instance } from "./instance.js";
 
 export const checkout = async (req, res) => {
   // res.header("Access-Control-Allow-Origin", "https://flipkart3.vercel.app");
+  console.log("Razorpay API Key:", process.env.RAZORPAY_API_KEY);
+  console.log("Razorpay API Secret:", process.env.RAZORPAY_API_SECRET);
+
   const instance = new Razorpay({
     key_id: process.env.RAZORPAY_API_KEY,
     key_secret: process.env.RAZORPAY_API_SECRET,
   });
+
   console.log("before options creation");
   var options = {
     amount: Number(req.body.amount * 100), // amount in the smallest currency unit
@@ -27,40 +30,36 @@ export const checkout = async (req, res) => {
   console.log("before order creation");
 
   try {
-  const order = await instance.orders.create(options);
-  console.log("order created");
-  res.status(200).json({
-    success: true,
-    order,
-  });
-} catch (error) {
-  console.error("order failed", error);
-  res.status(400).json({
-    success: false,
-    order: "order not created",
-  });
-}
-
-  
+    const order = await instance.orders.create(options);
+    console.log("order created");
+    res.status(200).json({
+      success: true,
+      order,
+    });
+  } catch (error) {
+    console.error("order failed", error);
+    res.status(400).json({
+      success: false,
+      order: "order not created",
+    });
+  }
 };
 
 export const paymentVerification = async (req, res) => {
-  const {razorpay_payment_id,razorpay_order_id,razorpay_signature} = req.body;
+  const { razorpay_payment_id, razorpay_order_id, razorpay_signature } =
+    req.body;
   const order_id = razorpay_order_id;
 
-  const generated_signature = hmac_sha256(order_id + "|" + razorpay_payment_id, process.env.RAZORPAY_API_SECRET);
+  const generated_signature = hmac_sha256(
+    order_id + "|" + razorpay_payment_id,
+    process.env.RAZORPAY_API_SECRET
+  );
 
   if (generated_signature == razorpay_signature) {
-    
-
-    res.redirect(`/paymentsuccess?reference=${razorpay_payment_id}`)
-  }
-  else{
+    res.redirect(`/paymentsuccess?reference=${razorpay_payment_id}`);
+  } else {
     res.status(400).json({
       success: false,
     });
   }
-
-
-  
 };
